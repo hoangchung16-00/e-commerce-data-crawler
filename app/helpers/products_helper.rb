29ba0ff421fcd6e53dd1a6c0d58d0246525ebfc1
hoangchung16-00 <hoangchung1687@gmail.com>
2026-07-11 23:09:09 +0700

    Add helper for safe url

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,18 @@
 module ProductsHelper
+  def safe_product_url(url)
+    # Return nil if URL is blank
+    return nil if url.blank?
+
+    uri = begin
+      URI.parse(url)
+    rescue URI::InvalidURIError
+      return nil
+    end
+
+    # Only allow http and https schemes
+    return nil unless [ "http", "https" ].include?(uri.scheme)
+
+    # Return the validated URL as a string
+    url.to_s
+  end
 end
